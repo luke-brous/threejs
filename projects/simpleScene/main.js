9f271f0e79257a291184e8617d0e8a7579add93d
Luke Broussard <brouslr0@sewanee.edu>
2026-01-27 19:19:19 +0000

    started on project 1

---
+++ projects/simpleScene/main.js
@@ -0,0 +1,59 @@
+
+
+import * as THREE from 'three';
+import { TW } from 'tw';
+import GUI from 'https://cdn.jsdelivr.net/npm/lil-gui@0.20/+esm';
+import * as piece from './pieces.js';
+
+console.log(`Loaded Three.js version ${THREE.REVISION}`);
+
+// for debugging
+globalThis.THREE = THREE;
+globalThis.TW = TW;
+
+// Create an initial empty Scene
+var scene = new THREE.Scene();
+globalThis.scene = scene;
+
+// ================================================================
+// Scene
+
+
+const planeGeo = new THREE.PlaneGeometry(20,20);
+const planeMesh = new THREE.Mesh( planeGeo, new THREE.MeshBasicMaterial( {color: 'green', side: THREE.DoubleSide} ) );
+planeMesh.rotation.x = Math.PI / 2;
+planeMesh.position.y = -0.01;
+scene.add( planeMesh );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ===============================================================
+
+// Create a renderer to render the scene
+var renderer = new THREE.WebGLRenderer();
+
+// TW.mainInit() initializes TW, adds the canvas to the document,
+// enables display of 3D coordinate axes, sets up keyboard controls
+TW.mainInit(renderer,scene);
+
+const gui = new GUI();
+
+
+// Set up a camera for the scene
+var state = TW.cameraSetup(renderer,
+                           scene,
+                           {minx: -10, maxx: 10,
+                            miny: 0, maxy: 12,
+                            minz: -10, maxz: 10});
